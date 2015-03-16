6e6e09b5249283b0aa6352e1c0b46097bea521d4
serioroja <serioroja@gmail.com>
2015-03-15 19:44:34 -0700

    Test first commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/******************************************************************************
+ * hello - Display a message to the terminal and then terminate.
+ ******************************************************************************
+ * Author  Adonis
+ * Date    03/15/2015
+ ******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    printf("Hello World!\n");
+
+    return (EXIT_SUCCESS);
+}
